f56e23600b1927dbc6275e53cd0f64932f5e970a
mateuslph <mlptche@gmail.com>
2022-01-10 00:29:49 -0300

    add recursos html

--- php-html.php
+++ php-html.php
@@ -36,10 +36,43 @@ $contasCorrentes ['156.478.098-89'] = [
     'saldo' => 2000
 ];
 
+function exibeConta(array $conta)
+{
+    ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    echo "<li> Titular: $titular . Saldo: $saldo</li>";
+}
+
 unset($contasCorrentes['132.456.786-90']);
 
 foreach($contasCorrentes as $cpf => $conta) {
     list('titular' => $titular, 'saldo' => $saldo) = $conta;
     echo $cpf . $titular . $saldo . '<br>';
 }
+
+echo '<ul>';
+foreach($contasCorrentes as $cpf => $conta) {
+    exibeConta($conta);
+}
+echo '</ul>';
 ?>
+<html>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf8">
+    <title>Documento</title>
+</head>
+<body>
+    <h1>Contas correntes</h1>
+    <dl>
+        <?php foreach($contasCorrentes as $cpf => $conta) { ?>
+        <dt>
+            <h3><?= $conta['titular']; ?> - <?= $cpf; ?></h3>
+        </dt>
+        <dd>
+            Saldo: <?= $conta['saldo'] ?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
